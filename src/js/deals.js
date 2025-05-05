571e8eb6a108b529ff557caa8271606cb41d7b8f
Saqib Islam <saaqi.grw@gmail.com>
2025-05-05 23:43:06 +0500

    Refactor code structure for improved readability and maintainability

--- src/js/deals.js
+++ src/js/deals.js
@@ -58,55 +58,61 @@ if (alertBoxesElement) alertBoxesElement.innerHTML = generateAlertBoxHtml(alertB
  */
 const dealsSlidesData = [
   {
-    icon: `<i class="icon bx bx-wind display-6"></i>`,
-    title: `AC System Flush, Vacuum & Recharge Freon for $119.99!`,
-    body: `Beat the heat with our top-notch service! Get an AC System Flush, Vacuum, and Recharge Freon for just $119.99.`,
+    icon: `<i class="icon bx bx-color display-6"></i>`,
+    title: `Wheel Alignment starting at just $59.99`,
+    body: `🚗 Car pulling left or right? Steering feeling off? You might need a wheel alignment!
+For a limited time only, get your wheel alignment today!`,
+    price: 59.99
   },
   {
     icon: `<i class="icon bx bxs-car-wash display-6"></i>`,
-    title: `Synthetic Oil Change for just $49.99`,
-    body: `Get your car running smoothly with our oil change specials! Synthetic oil change for $49.99. Quality service at great prices!`,
-  },
-  {
-    icon: `<i class="icon bx bxs-car-wash display-6"></i>`,
-    title: `Conventional Oil Change for just $18.99`,
-    body: `Get your car running smoothly with our oil change specials! Conventional oil change for $18.99. Quality service at great prices!`,
+    title: `Wheel Alignment + Synthetic Oil Change for Just $79.99!`,
+    body: `Is your car pulling to one side? Feeling a little shaky on the road? It might be time for a wheel alignment! And while you're at it, give your engine the fresh start it deserves with a synthetic oil change.`,
+    price: 79.99
   },
   {
     icon: `<i class="icon bx bx-stop-circle display-6"></i>`,
-    title: `Brake Pads starting at just $79.99`,
-    body: `Enhance your vehicle's safety with our limited-time brake pads deal! Expert replacements, special pricing. Act now, drive confidently!`,
+    title: `Coolant + Brake Flush starting at just $79.99`,
+    body: `Did you know neglecting your coolant & brake fluid can lead to overheating, poor braking, and expensive repairs? 😨 Don't wait until it's too late!`,
+    price: 79.99
   },
   {
-    icon: `<i class="icon bx bx-color display-6"></i>`,
-    title: `Wheel Alignment starting at just $49.99`,
-    body: `Wheel alignment for $49.99 & up! Limited time offer. Improved handling and fuel efficiency. Visit us today!`,
+    icon: `<i class="icon bx bx-wind display-6"></i>`,
+    title: `AC System Flush starting at $99.99!`,
+    body: `Don't let Florida's heat turn your car into a sauna! ☀️ If your AC isn't cooling like it used to, it might be time for a coolant flush.`,
+    price: 99.99
+
   },
-  {
-    icon: `<i class="icon bx bxs-wrench display-6"></i>`,
-    title: `Full Tune Up Starting from just $249.99`,
-    body: `Tune up starting at just $249.99! Limited time offer. Optimize performance and reliability. Visit us today!`,
-  },
-  {
-    icon: `<i class="icon bx bxs-color display-6"></i>`,
-    title: `4 New Tires from $399.99 & Up`,
-    body: `Get rolling with a fantastic deal! 4 new tires for $399.99 & free wheel alignment. Limited time offer. Visit us now!`,
-  },
-  {
-    icon: `<i class="icon bx bxs-color display-6"></i>`,
-    title: `4 Used Tires from $199.99 & Up`,
-    body: `Get road-ready with our special deal! 4 used tires for $199.99 & free wheel alignment. Limited time offer. Visit us now!`,
-  },
-  {
-    icon: `<i class="icon bx bxs-car-garage display-6"></i>`,
-    title: `FREE Check Engine Light diagnostics.`,
-    body: `Worried about your check engine light? Get a FREE diagnostic from our expert technicians and drive with confidence!`,
-  },
-  {
-    icon: `<i class="icon bx bxs-car-garage display-6"></i>`,
-    title: `Free Vehicle Inspection with any service.`,
-    body: `Comprehensive vehicle inspection for FREE with any service! Limited time offer. Visit us today!`,
-  },
+  // {
+  //   icon: `<i class="icon bx bxs-car-wash display-6"></i>`,
+  //   title: `Conventional Oil Change for just $18.99`,
+  //   body: `Get your car running smoothly with our oil change specials! Conventional oil change for $18.99. Quality service at great prices!`,
+  // },
+  // {
+  //   icon: `<i class="icon bx bxs-wrench display-6"></i>`,
+  //   title: `Full Tune Up Starting from just $249.99`,
+  //   body: `Tune up starting at just $249.99! Limited time offer. Optimize performance and reliability. Visit us today!`,
+  // },
+  // {
+  //   icon: `<i class="icon bx bxs-color display-6"></i>`,
+  //   title: `4 New Tires from $399.99 & Up`,
+  //   body: `Get rolling with a fantastic deal! 4 new tires for $399.99 & free wheel alignment. Limited time offer. Visit us now!`,
+  // },
+  // {
+  //   icon: `<i class="icon bx bxs-color display-6"></i>`,
+  //   title: `4 Used Tires from $199.99 & Up`,
+  //   body: `Get road-ready with our special deal! 4 used tires for $199.99 & free wheel alignment. Limited time offer. Visit us now!`,
+  // },
+  // {
+  //   icon: `<i class="icon bx bxs-car-garage display-6"></i>`,
+  //   title: `FREE Check Engine Light diagnostics.`,
+  //   body: `Worried about your check engine light? Get a FREE diagnostic from our expert technicians and drive with confidence!`,
+  // },
+  // {
+  //   icon: `<i class="icon bx bxs-car-garage display-6"></i>`,
+  //   title: `Free Vehicle Inspection with any service.`,
+  //   body: `Comprehensive vehicle inspection for FREE with any service! Limited time offer. Visit us today!`,
+  // },
 ];
 
 /**
@@ -117,15 +123,25 @@ const dealsSlidesData = [
  */
 const generateDealsSlidesHtml = (slidesData) => {
   return slidesData.map((sl) => {
+    const {
+      icon = '',
+      title = '',
+      body = '',
+      price = ''
+    } = sl;
+
     const output =
       `<li class="glide__slide h-auto">` +
         `<div class="ag-courses_item h-100 bg-primary p-3 text-light rounded">` +
           `<div class="ag-courses-item_bg"></div>` +
           `<div class="ag-courses-item_title fs-5 fw-medium mb-2">` +
-            `<div>${sl.icon}</div>` +
-            `<div>${sl.title}</div>` +
+            `<div>${icon}</div>` +
+            `<div>${title}</div>` +
           `</div>` +
-          `<div class="ag-course-body">${sl.body}</div>` +
+          `<div class="ag-course-body">
+            <p>${body}</p>
+            ${price ? `<div class="btn btn-outline-light fs-3 py-1 px-3">$ ${price.toFixed(2)}</div>` : ''}
+          </div>` +
         `</div>` +
       `</li>`;
     return output;
--- src/js/testimonials.js
+++ src/js/testimonials.js
@@ -7,7 +7,7 @@
 const alertBoxesData = [
   {
     url: `https://goo.gl/maps/zA9qfkUA6gyxb3Ff9`,
-    img: `./media/reviewers/xavier_marin.webp`,
+    img: `xavier_marin.webp`,
     name: `Xavier Marin`,
     body: `I brought my 2009 Toyota Highlander here with 239,000 miles for a Diagnostic after taking it to 3 other mechanics even paying twice to replace sensors, because that’s why they said. . Car Club Tire found multiple things wrong and showed dedication and patience with my vehicle to ensure it was fixed. The guys are honest, friendly, and understanding. They exceeded the service on my vehicle. I left with my vehicle with a smile. I need some other work done soon on suspension and my wife’s truck. I’m bringing my family’s vehicles here from now on. I’m happy with Car Club Tires. Thank you so much!`,
   },
@@ -25,7 +25,7 @@ const alertBoxesData = [
   },
   {
     url: `https://goo.gl/maps/ria7TBqB4D7uDChx8`,
-    img: `./media/reviewers/usman_shah.webp`,
+    img: `usman_shah.webp`,
     name: `Usman Shah`,
     body: `I was driving down to Miami and my car broke down in the evening. Car repair shops near me were closing and offered to assist the next morning. I was lucky enough to call this shop. If I remember correctly, the owner was Farhan, and he had my car fixed in no time. It was a wonderful experience. The team is amazing. They also offered complimentary snacks, hands down the best service ever.`,
   },
@@ -37,7 +37,7 @@ const alertBoxesData = [
   },
   {
     url: `https://maps.app.goo.gl/Kh5UKYhqnJr9vhy59`,
-    img: `./media/reviewers/Virag_Anna_Szolgyemy.webp`,
+    img: `Virag_Anna_Szolgyemy.webp`,
     name: `Virag Anna Szolgyemy`,
     body: `We were able to do a walk-in and get our alignment done right away. It took 45 mins from start to finish and it was only $60. All the staff was very friendly and helpful. It’s too bad I don’t live around here or else I would be a regular for sure :)`,
   },
@@ -49,31 +49,31 @@ const alertBoxesData = [
   },
   {
     url: `https://goo.gl/maps/yAAVWdtiPJ9v8y5U7`,
-    img: `./media/reviewers/Africans_In_Orlando.webp`,
+    img: `Africans_In_Orlando.webp`,
     name: `Africans In Orlando`,
     body: `When you're looking for honest price, honest diagnostic, weekend service and great customer service, these guys are the best in the area. Shawn and Forhan are amazing professionals who will take good care of you and your car. I'll be coming here on a regular now 🙂 Happy Customer!`,
   },
   {
     url: `https://goo.gl/maps/P5FHwGYWsxGWWU3R7`,
-    img: `./media/reviewers/Eddrina-Delmont.webp`,
+    img: `Eddrina-Delmont.webp`,
     name: `Eddrina Delmont`,
     body: `Amazing amazing ! Fixed my breaks in no time. Customer service was excellent. Definitely recommend for anyone who doesn’t want to get ripped off or overpriced by other dealers. Come in and they will take care of you. Thank you so much Farhan for helping me out today and in a timely manner!`,
   },
   {
     url: `https://maps.app.goo.gl/6gwbvcAP8LdTyMnb6`,
-    img: `./media/reviewers/Danielle_Marie.webp`,
+    img: `Danielle_Marie.webp`,
     name: `Danielle Marie`,
     body: `I just bought a used vehicle and a day later turns out I need brakes. I found a Car Club Tire & auto on Groupon. I drove 51 minutes with no brakes just to get there. Thinking I just needed brake pads I was not financially prepared to afford much more than that. Turns out I need orders also. I'm a single mom and can't afford it. Everyone there was super helpful and did their best to help me and found the right option for my situation and I left with all 4 new brake pads and 2 new front rods. I won't be going anywhere else from now on. Everyone was so nice and very professional and treated me very kindly. Thank you so much 👍😊`,
   },
   {
     url: `https://goo.gl/maps/SJc9B8wmXW4ezHcF6`,
-    img: `./media/reviewers/Lex_Scooter.webp`,
+    img: `Lex_Scooter.webp`,
     name: `Lex Scooter`,
     body: `I will always be back to this location for service on my vehicles . I read a review for them that read "this location restored their faith in humanity" & I couldn't agree more. Thank you for doing ,all y'all can do ,for your customers. Fair prices & they gave me a fair quote over the phone , WITH EASE, after calling around for days to find the best deal on brakes .. I went the next day , around a general time i thought I could make it & it was one of the best experiences I've had at an auto repair shop. Can't thank them enough super trust worthy, Will be !!`,
   },
   {
     url: `https://goo.gl/maps/hDdZ7p2JEXzQrLyB9`,
-    img: `./media/reviewers/Mercy_Ares.webp`,
+    img: `Mercy_Ares.webp`,
     name: `Mercy Ares`,
     body: `Found this place on Groupon, they take appts & walk-ins. Arrived and was serviced immediately, the staff employees are professional and courteous. Amazing customer service experience. Super clean facility with an indoor AC comfortable waiting area, very clean bathrooms and free coffee! Was well worth the over an hour drive to get here but I'll definitely be returning even without Groupon to fix other issues on both my minivan & suv!`,
   },
@@ -97,13 +97,20 @@ const alertBoxesData = [
   },
 ]
 
+const reviewerPics = import.meta.glob('../assets/media/reviewers/**/*', { eager: true });
+import fallBackImg from '../assets/media/cc-logo.svg';
+
 const aletBoxHtml = alertBoxesData.map((rv) => {
-  const avatar = rv.img
-    ? `<img decoding="async" loading="lazy" width="26" height="26" src="${rv.img}" alt="Google Reviewer's Photo">`
+  const {
+    url = '',
+    img = '',
+    name = '',
+    body = ''
+  } = rv;
+  const coverImage = reviewerPics[`../assets/media/reviewers/${img}`]?.default || fallBackImg;
+  const avatar = img
+    ? `<img decoding="async" loading="lazy" width="26" height="26" src="${coverImage}" alt="Google Reviewer's Photo">`
     : `<i class='bx bx-user p-1 border rounded-circle'></i>`
-  const url = rv.url ? rv.url : ``
-  const name = rv.name ? rv.name : ``
-  const body = rv.body ? rv.body : ``
 
   const output =
   `<li class="glide__slide h-auto">`+
